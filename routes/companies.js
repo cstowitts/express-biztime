5c83600b7cf3fc2d9daa144ca1d4274d0efbc753
brendan <lobrendan@gmail.com>
2022-02-17 12:08:55 -0800

    working on a few of the companies routes

--- routes/companies.js
+++ routes/companies.js
@@ -1,20 +1,33 @@
 const express = require("express");
+const { NotFoundError } = require("../expressError");
 
 const db = require("../db");
 const router = new express.Router();
 
 
 /** Returns list of companies, like {companies: [{code, name}, ...]} */
-router.get("/", function(req, res){
-
+router.get("/", function (req, res) {
+  const results = db.query(
+    `SELECT code, name, description
+    FROM companies`);
+  const companies = results.rows;
+  return res.json({ companies });
 })
+
 
 /** Return obj of company: {company: {code, name, description}}
  * If the company given cannot be found, 
  * this should return a 404 status response. 
  * */
-router.get("/:code", function(req, res){
-
+router.get("/:code", async function (req, res) {
+  const code = req.params.code;
+  const results = await db.query(
+    `SELECT code, name, description
+    FROM companies
+    WHERE code = $1`, [code]
+  );
+  const companies = results.rows[0];
+  return res.json({ companies })
 })
 
 
@@ -22,7 +35,7 @@ router.get("/:code", function(req, res){
  * Needs to be given JSON like: {code, name, description}
  * Returns obj of new company: {company: {code, name, description}}
  * */
-router.post("/", function(req, res){
+router.post("/", function (req, res) {
 
 })
 
@@ -31,7 +44,7 @@ router.post("/", function(req, res){
  * Needs to be given JSON like: {name, description}
  * Returns update company object: {company: {code, name, description}}
  * */
-router.put("/:code", function(req, res){
+router.put("/:code", function (req, res) {
 
 })
 
@@ -40,7 +53,7 @@ router.put("/:code", function(req, res){
  * Should return 404 if company cannot be found.
  * Returns {status: "deleted"}
  * */
-router.delete("/:code", function(req, res){
+router.delete("/:code", function (req, res) {
 
 })
 
